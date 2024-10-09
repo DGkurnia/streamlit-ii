98276d42bac9d5d64066dc93fc306ea49e676344
Derfansyah Guswiranata Kurnia <111730680+DGkurnia@users.noreply.github.com>
2024-10-10 06:02:58 +0700

    Add files via upload
    let's see the result shall we

--- dashboard.py
+++ dashboard.py
@@ -42,3 +42,48 @@ with st.sidebar:
         max_value=max_date,
         value=[min_date, max_date]
     )
+#persiapan grafik partikulat
+st.header('Air Quality in Aotizhongxin :sparkles:')
+#Data utama
+main_df = beijingdf[(beijingdf["datetime"] >= str(start_date)) & 
+                (beijingdf["datetime"] <= str(end_date))]
+#Deklarasi sub kepala
+st.subheader("Inspeksi Partikulat")
+tahunan = main_df.groupby("datetime").mean(numeric_only=True)
+
+#pemilihan stasiun
+pilihan = st.selectbox("Pilihan stasiun", tahunan['station'].unique())
+# Filtrasi data
+filtrat = tahunan[tahunan['station'] == pilihan]
+
+# Deklarasi tangga
+filtrat.set_index('datetime', inplace=True)
+
+#Grafik partikulat untuk Inspeksi partikulat (nilai PM2.5 & nilai PM10)
+[anPMa, dlPMa, anPMb, dlPMb] = [40, 150, 35, 75] 
+#Batas aman
+safety_limits = {'PM2.5 anual': anPMa,  'PM2.5 maksimal': dlPMa, # Batas anual dan Maksimal PM 2.5   
+                 'PM10 anual': anPMb, 'PM10 maksimal': dlPMb} #Batas anual dan maksimal PM.10
+#Keterangan (1. a adalah inepeksi PM2.5 b adalah PM10 2. kode an adalah anual kode ma adalah nilai maksimal)
+fig = px.line(hasil, x='datetime', y=['PM2.5', 'PM10'],
+              labels={['PM2.5','PM10']: 'Kadar Partikulat', 'datetime': 'Tanggal'},
+              title='Inspeksi Partikulat')
+#Inspeksi Batas Aman
+for pollutant, limit in safety_limits.items():
+    fig.add_hline(y=limit, line_color="red", line_dash="dash",
+                   annotation_text=f"{pollutant} Batas aman: {limit} µg/m³", 
+                   annotation_position="top right")
+#Ilustrasi normal untuk partikulat
+st.plotly_chart(fig)
+#Pemeriksaan batas partikulat
+batasanu = hasil[(hasil['PM2.5'] > safety_limits['PM2.5 anual'] & hasil['PM2.5'] < safety_limits['PM2.5 maksimal']) 
+                 | (hasil['PM10'] > safety_limits['PM10 anual'] & hasil['PM10'] < safety_limits['10 maksimal'])] #Batas anual
+lwt = hasil[(hasil['PM2.5'] > safety_limits['PM2.5 maksimal']) | (hasil['PM10'] > safety_limits['PM10 maksimal'])] #Batas maksimal
+#Area batas anual
+if not lwt.empty:
+    st.warning("Peringatan : Ini sudah terlalu bahaya")
+elif not batasanu.empty and lwt.empty:
+    st.warning("Peringatan : Ini harus hati-hati") #Kasus melewati batas anual (tapi di dibawah nilai maksimal)
+else:
+    st.success("Kadar udara masih aman")
+ #------------------------------------------
